0b10bd48ddee239f65e9df7e0dd79b035740c0d9
Lieuwe B. Leene <11837884+lleene@users.noreply.github.com>
2020-08-23 12:21:33 +0200

    Create packages_DevOps.sh

---
+++ build_scripts/packages_DevOps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Package list exported from blue print conf.
+
+cmake npm make gcc curl unzip mlocate zlib bzip2 openssl ImageMagick flex bison ragel git sendmail openssh mkvtoolnix coreutils automake ncurses postgresql-contrib  postgresql postgresql-server glib2 nginx
+
+php php-common php-json php-xml php-intl php-pdo php-pear php-mbstring php-mysqlnd php-pecl-apcu php-pecl-zip php-opcache php-fpm php-gd php-cli php-process php-ldap php-pecl-imagick
+
+libpq-devel libpng-devel libexif-devel libxml2-devel libxslt-devel  libcurl-devel readline-devel  giflib-devel expat-devel glib2-devel lcms2-devel libjpeg-turbo-devel  librsvg2-devel orc-devel fftw-devel libwebp-devel libimagequant-devel libtiff-devel libgsf-devel cfitsio-devel zlib-devel bzip2-devel ImageMagick-devel ncurses-devel openssl-devel postgresql-server-devel php-devel
